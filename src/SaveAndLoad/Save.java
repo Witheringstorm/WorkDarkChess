5d6dcd8b570ddd3b854176ac464c52d91aba95b6
Witheringstorm <K38663279@163.com>
2022-12-26 16:58:03 +0800

    Update

--- src/SaveAndLoad/Save.java
+++ src/SaveAndLoad/Save.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import EventDealer.*;
 
@@ -33,6 +34,7 @@ public class Save {
         record.add(",ClickInfo" + " " + Player.click_times + " " + Player.playerTurn + " " + ClickPieces.theVeryFirstClick + "!");
 
     }
+
     //记录棋盘末状态
     public static void writeRecord2() {
         StringBuilder sb = new StringBuilder();
@@ -46,6 +48,7 @@ public class Save {
         record2.add(sb.toString());
         record2.add(",ClickInfo" + " " + Player.click_times + " " + Player.playerTurn + " " + ClickPieces.theVeryFirstClick + "!");
     }
+
     //将记录写入用户指定的文件
     public static void writeFile(String path) {
         try {
@@ -58,7 +61,7 @@ public class Save {
                 System.out.println(line);
                 writer.flush();
             }
-           lines = record2;
+            lines = record2;
             for (String line : lines
             ) {
                 writer.write(line);
@@ -89,6 +92,7 @@ public class Save {
 
 
     public static int loadGame(String path) {
+        AtomicBoolean moveCorrect = new AtomicBoolean(true);
         ClickPieces.interact_click = null;
         ClickPieces.theVeryFirstClick = true;
         String input = fileReader(path);
@@ -143,92 +147,98 @@ public class Save {
             }
         }
 
-        String[] action = part[2].split("&");
-        for (String a : action) {
-            String[] data = a.split(" ");
-            if (data[0] != "") {
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 4; j++) {
-                        chessboard[i][j].x = j;
-                        chessboard[i][j].y = i;
+            String[] action = part[2].split("&");
+            for (String a : action) {
+                String[] data = a.split(" ");
+                if (data[0] != "") {
+                    for (int i = 0; i < 8; i++) {
+                        for (int j = 0; j < 4; j++) {
+                            chessboard[i][j].x = j;
+                            chessboard[i][j].y = i;
+                        }
+                    }
+                    int click_x = Integer.parseInt(data[0]);
+                    int click_y = Integer.parseInt(data[1]);
+                    Piece click = chessboard[click_y][click_x];
+                    ClickPieces.click(click);
+
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
                     }
                 }
-                int click_x = Integer.parseInt(data[0]);
-                int click_y = Integer.parseInt(data[1]);
-                Piece click = chessboard[click_y][click_x];
-                ClickPieces.click(click);
             }
-        }
-        Points.calculatePoints();
-        record = new ArrayList<>();
-        record2 = new ArrayList<>();
-        writeRecord();
+            Points.calculatePoints();
+            record = new ArrayList<>();
+            record2 = new ArrayList<>();
+            writeRecord();
 
 
-        String[] pieces2 = part[3].split(",");
-        for (String piece : pieces2) {
-            String[] data = piece.split(" ");
-            if (!data[0].equals("") && !data[0].equals("ClickInfo")) {
-                int type = Integer.parseInt(data[0]);
-                char side = data[1].charAt(0);
-                boolean alive = Boolean.parseBoolean(data[2]);
-                int y = Integer.parseInt(data[3]);
-                int x = Integer.parseInt(data[4]);
-                boolean IsReversal = Boolean.parseBoolean(data[5]);
-                if (type == 0) {
-                    chessboardCopy[x][y] = new CannonChess(side);
-                    chessboardCopy[x][y].alive = alive;
-                    chessboardCopy[x][y].IsReversal = IsReversal;
-                } else if (type == 1) {
-                    chessboardCopy[x][y] = new SoldierChess(side);
-                    chessboardCopy[x][y].alive = alive;
-                    chessboardCopy[x][y].IsReversal = IsReversal;
-                } else if (type == 2) {
-                    chessboardCopy[x][y] = new HorseChess(side);
-                    chessboardCopy[x][y].alive = alive;
-                    chessboardCopy[x][y].IsReversal = IsReversal;
-                } else if (type == 3) {
-                    chessboardCopy[x][y] = new ChariotChess(side);
-                    chessboardCopy[x][y].alive = alive;
-                    chessboardCopy[x][y].IsReversal = IsReversal;
-                } else if (type == 4) {
-                    chessboardCopy[x][y] = new MinisterChess(side);
-                    chessboardCopy[x][y].alive = alive;
-                    chessboardCopy[x][y].IsReversal = IsReversal;
-                } else if (type == 5) {
-                    chessboardCopy[x][y] = new AdvisorChess(side);
-                    chessboardCopy[x][y].alive = alive;
-                    chessboardCopy[x][y].IsReversal = IsReversal;
-                } else if (type == 6) {
-                    chessboardCopy[x][y] = new GeneralChess(side);
-                    chessboardCopy[x][y].alive = alive;
-                    chessboardCopy[x][y].IsReversal = IsReversal;
+            String[] pieces2 = part[3].split(",");
+            for (String piece : pieces2) {
+                String[] data = piece.split(" ");
+                if (!data[0].equals("") && !data[0].equals("ClickInfo")) {
+                    int type = Integer.parseInt(data[0]);
+                    char side = data[1].charAt(0);
+                    boolean alive = Boolean.parseBoolean(data[2]);
+                    int y = Integer.parseInt(data[3]);
+                    int x = Integer.parseInt(data[4]);
+                    boolean IsReversal = Boolean.parseBoolean(data[5]);
+                    if (type == 0) {
+                        chessboardCopy[x][y] = new CannonChess(side);
+                        chessboardCopy[x][y].alive = alive;
+                        chessboardCopy[x][y].IsReversal = IsReversal;
+                    } else if (type == 1) {
+                        chessboardCopy[x][y] = new SoldierChess(side);
+                        chessboardCopy[x][y].alive = alive;
+                        chessboardCopy[x][y].IsReversal = IsReversal;
+                    } else if (type == 2) {
+                        chessboardCopy[x][y] = new HorseChess(side);
+                        chessboardCopy[x][y].alive = alive;
+                        chessboardCopy[x][y].IsReversal = IsReversal;
+                    } else if (type == 3) {
+                        chessboardCopy[x][y] = new ChariotChess(side);
+                        chessboardCopy[x][y].alive = alive;
+                        chessboardCopy[x][y].IsReversal = IsReversal;
+                    } else if (type == 4) {
+                        chessboardCopy[x][y] = new MinisterChess(side);
+                        chessboardCopy[x][y].alive = alive;
+                        chessboardCopy[x][y].IsReversal = IsReversal;
+                    } else if (type == 5) {
+                        chessboardCopy[x][y] = new AdvisorChess(side);
+                        chessboardCopy[x][y].alive = alive;
+                        chessboardCopy[x][y].IsReversal = IsReversal;
+                    } else if (type == 6) {
+                        chessboardCopy[x][y] = new GeneralChess(side);
+                        chessboardCopy[x][y].alive = alive;
+                        chessboardCopy[x][y].IsReversal = IsReversal;
+                    }
                 }
             }
-        }
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 4; j++) {
-                chessboardCopy[i][j].x = j;
-                chessboardCopy[i][j].y = i;
-            }
-        }
-        boolean moveCorrect = true;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (!chessboardCopy[i][j].toString().equals(chessboard[i][j].toString())) {
-                    moveCorrect = false;
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 4; j++) {
+                    chessboardCopy[i][j].x = j;
+                    chessboardCopy[i][j].y = i;
                 }
             }
-        }
-        SwingUtilities.invokeLater(() -> {
-            ChessGameFrame g = new ChessGameFrame();
-            g.canSave = false;
-            g.setVisible(true);
-        });
+
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 4; j++) {
+                    if (!chessboardCopy[i][j].toString().equals(chessboard[i][j].toString())) {
+                        moveCorrect.set(false);
+                    }
+                }
+            }
+//            SwingUtilities.invokeLater(() -> {
+//            ChessGameFrame g = new ChessGameFrame();
+//            g.canSave = false;
+//            g.setVisible(true);
+//        });
         if (!part[4].equals("r") && !part[4].equals("b")) {
             return 104;
         }
-        if (!moveCorrect) {
+        if (!moveCorrect.get()) {
             return 105;
         } else return 0;
     }
@@ -304,11 +314,7 @@ public class Save {
             }
         }
         Points.calculatePoints();
-        SwingUtilities.invokeLater(() -> {
-            ChessGameFrame g = new ChessGameFrame();
-            g.setVisible(true);
-            g.canSave2 = true;
-        });
+
     }
 }
 
